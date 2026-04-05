f59b15e89225e5ceeb9c97094ceb6cdaddaf6442
Noha Shehab <nohashehab.iti@gmail.com>
2026-04-05 11:15:20 +0200

    exceptions

--- main.py
+++ main.py
@@ -15,6 +15,27 @@ if __name__ == '__main__':
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
+#
+# temp  = 'We are {no_stds} in OS track'
+# print(temp.format(no_stds=30))
 
-temp  = 'We are {no_stds} in OS track'
-print(temp.format(no_stds=30))
+def sum_num():
+    try:
+        num1 = int(input("please enter num1: "))
+        num2 = int(input("please enter num2: "))
+        res = num1 / num2
+    except Exception as  e:
+        print(e)
+        res = math.nan
+        return res
+    else:
+        print(res)
+        return res
+
+    finally:
+        # execution of finally preceeds return
+        print("=== Marked safe from this function ===")
+    print("***************************************************")
+
+
+print(sum_num())
